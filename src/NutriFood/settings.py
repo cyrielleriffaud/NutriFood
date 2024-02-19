5b8108d93e822c3c50c1d2de45ff1017258dba5d
Cyrielle Riffaud <cycy.riff@gmail.com>
2024-02-19 16:17:09 +0100

    Mise à jour du fichier settings.py

--- src/NutriFood/settings.py
+++ src/NutriFood/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "NutriFood.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NutriFood',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
